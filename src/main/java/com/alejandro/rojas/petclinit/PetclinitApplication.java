e66fb0c3980d8dff0de5bdca126d55fd117fb5f8
Alejandro Andrade <alejo.andrade.rojas@gmail.com>
2021-06-03 18:15:45 -0300

    Initial project Add

---
+++ src/main/java/com/alejandro/rojas/petclinit/PetclinitApplication.java
@@ -0,0 +1,13 @@
+package com.alejandro.rojas.petclinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetclinitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alejandro/rojas/petclinit/PetclinitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandro.rojas.petclinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
