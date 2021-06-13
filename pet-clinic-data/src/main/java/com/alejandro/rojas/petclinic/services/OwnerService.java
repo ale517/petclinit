596e0398ccf3ae44f43f3b099d37ab5ee871f71d
Alejandro Andrade <alejo.andrade.rojas@gmail.com>
2021-06-13 18:56:51 -0300

    refactor the common service interface , closes #11

--- pet-clinic-data/src/main/java/com/alejandro/rojas/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/alejandro/rojas/petclinic/services/OwnerService.java
@@ -2,16 +2,6 @@ package com.alejandro.rojas.petclinic.services;
 
 import com.alejandro.rojas.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
-
-    Owner findByLastName();
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
-
+public interface OwnerService extends  CrudService<Owner,Long>{
+    Owner findByLastName(String lastName);
 }
--- pet-clinic-data/src/main/java/com/alejandro/rojas/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/alejandro/rojas/petclinic/services/PetService.java
@@ -1,13 +1,6 @@
 package com.alejandro.rojas.petclinic.services;
+
 import com.alejandro.rojas.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService  extends  CrudService<Pet,Long>{
 }
